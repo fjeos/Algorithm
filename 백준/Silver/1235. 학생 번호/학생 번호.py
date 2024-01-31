4ddae895cbdc9f703de6ea25bbcc9889ba5c66cf
nayoung <97019549+fjeos@users.noreply.github.com>
2024-01-31 16:10:56 +0900

    [Silver IV] Title: 학생 번호, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1235. 학생 번호/학생 번호.py
+++ 백준/Silver/1235. 학생 번호/학생 번호.py
@@ -1,18 +1,13 @@
 N = int(input())
-stu = []
+students = [input() for _ in range(N)]
+lists = ["" for _ in range(N)]
+isDup = True
 
-for _ in range(N):
-    stu.append(input())
-    
-length = len(stu[0])
-
-for i in range(1, length+1):
-    result = []
-    for j in range(N):
-        if stu[j][-i:] in result:
-            break
-        else:
-            result.append(stu[j][-i:])
-    if len(result) == N:
-        print(i)
+M = len(students[0])
+for j in range(M-1, -1, -1):
+    for i in range(N):
+        lists[i] += students[i][j]
+    if len(lists) == len(set(lists)):
         break
+         
+print(len(lists[i]))
