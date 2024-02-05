b27ee3bcd8605705cabc7f96da7d04a78623f1f9
nayoung <97019549+fjeos@users.noreply.github.com>
2024-02-05 13:15:40 +0900

    [Silver V] Title: 나이순 정렬, Time: 4136 ms, Memory: 48188 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,9 @@
+T = int(input())
+lists = []
+for _ in range(T):
+    age, name = input().split()
+    lists.append([int(age), name])
+
+lists.sort(key = lambda x:x[0])
+for i in lists:
+    print(' '.join(map(str, i)))
