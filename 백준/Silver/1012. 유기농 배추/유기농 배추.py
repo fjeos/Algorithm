748a1385d2b460f4ee0d94a142593a20923d8152
nayoung <97019549+fjeos@users.noreply.github.com>
2024-02-05 16:05:35 +0900

    [Silver II] Title: 유기농 배추, Time: 80 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,25 +1,27 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 for _ in range(int(input())):
-    
     M, N, K = map(int, input().split())
     field = [[0] * M for _ in range(N)]
     visited = [[False] * M for _ in range(N)]
+    
     dx = [0, 0, -1, 1]
     dy = [-1, 1, 0, 0]
     
     for i in range(K):
         y, x = map(int, input().split())
         field[x][y] = 1
-        
     result = []
+    worm = 0
+    
     queue = deque()
     for k in range(N):
         for l in range(M):
             if field[k][l] == 0 or visited[k][l]:
                 continue
             queue.append((k, l))
-            count = 1
             while queue:
                 x, y = queue.popleft()
                 visited[x][y] = True
@@ -30,7 +32,6 @@ for _ in range(int(input())):
                     if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and field[nx][ny]:
                         queue.append((nx, ny))
                         visited[nx][ny] = True
-                        count += 1
-            result.append(count)
+            worm += 1
             
-    print(len(result))
+    print(worm)
