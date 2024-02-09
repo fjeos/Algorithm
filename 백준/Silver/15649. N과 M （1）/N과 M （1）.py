e308460ba859778df0a6a4880588cdc2066ddfbc
nayoung <97019549+fjeos@users.noreply.github.com>
2024-02-09 18:23:22 +0900

    [Silver III] Title: N과 M (1), Time: 144 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -3,15 +3,18 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 result = []
+visited = [False] * (N+1)
 
 def dp():    
     if len(result) == M:
         print(' '.join(map(str, result)))
         return
     for i in range(1, N+1):
-        if i not in result:
+        if not visited[i]:
             result.append(i)
+            visited[i] = True
             dp()
             result.pop()
-        
+            visited[i] = False
+            
 dp()
