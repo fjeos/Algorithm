373f36b229d0f765bf7c9da837d22af3c14ae3c0
nayoung <97019549+fjeos@users.noreply.github.com>
2024-04-18 14:27:20 +0900

    [Easy] Title: 1. Two Sum, Time: 490 ms (38.39%), Space: 17.4 MB (84.21%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.py
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.py
@@ -1,11 +1,5 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        left, right = 0, 1
-        while left < len(nums) - 1:
-            if right == len(nums):
-                left += 1
-                right = left + 1
-                continue
-            if nums[left] + nums[right] == target:
-                return [left, right]
-            right += 1
+        for i in range(len(nums)):
+            if target - nums[i] in nums and i != nums.index(target - nums[i]):
+                return [i, nums.index(target - nums[i])]
