c6f57aa492c7adf8e8c46b09ffb90a96d618e0cc
nayoung <97019549+fjeos@users.noreply.github.com>
2024-04-18 23:29:49 +0900

    [Medium] Title: 15. 3Sum, Time: 2639 ms (5.00%), Space: 20.7 MB (51.64%) - LeetHub

--- LeetCode/Medium/0015-3sum/0015-3sum.py
+++ LeetCode/Medium/0015-3sum/0015-3sum.py
@@ -2,25 +2,21 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         answer = []
+        dic = defaultdict(int)
         for i in range(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
             left, right = i + 1, len(nums) - 1
-
             while left < right:
                 summ = nums[i] + nums[left] + nums[right]
-                if summ > 0:
+                if summ == 0:
+                    temp = [nums[i], nums[left], nums[right]]
+                    if dic[str(temp)] == 0:
+                        dic[str(temp)] = 1
+                        answer.append(temp)
+                    left += 1
                     right -= 1
                 elif summ < 0:
                     left += 1
                 else:
-                    answer.append([nums[i], nums[left], nums[right]])
-
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    left += 1
                     right -= 1
                 
         return answer
