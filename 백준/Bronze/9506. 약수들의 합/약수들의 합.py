1d0cd6b6197a2c2bd05616b703a5abd0b88d13eb
nayoung <97019549+fjeos@users.noreply.github.com>
2024-04-19 19:43:19 +0900

    [Bronze I] Title: 약수들의 합, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.py
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -3,15 +3,5 @@ while True:
     if n == -1:
         break
 
-    divisor = []
-    for i in range(1, n):
-        if n % i == 0:
-            divisor.append(i)
-
-    summ = 0
-    for i in divisor:
-        summ += i
-    if summ == n:
-        print(f"{n} = {' + '.join(map(str, divisor))}")
-    else:
-        print(f"{n} is NOT perfect.")
+    divisor = [i for i in range(1, n) if n % i == 0]
+    print(f"{n} = {' + '.join(map(str, divisor))}" if sum(divisor) == n else f"{n} is NOT perfect.")
