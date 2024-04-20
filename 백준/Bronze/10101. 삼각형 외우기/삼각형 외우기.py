ba105dbbc940de0f4eec0eb4316ed3c6aacbe1c0
nayoung <97019549+fjeos@users.noreply.github.com>
2024-04-20 16:27:52 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
@@ -1,10 +1,4 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
 lists = [int(input()) for _ in range(3)]
-lists.sort()
-if lists[0] == lists[1] == lists[2] == 60:
-    print("Equilateral")
-elif sum(lists) == 180 and (lists[0] == lists[1] or lists[1] == lists[2]):
-    print("Isosceles")
-elif sum(lists) == 180 and (lists[0] != lists[1] or lists[1] != lists[2]):
-    print("Scalene")
-else:
-    print("Error")
+print("Error" if sum(lists) != 180 else ["Equilateral", "Isosceles", "Scalene"][len(set(lists))-1])
