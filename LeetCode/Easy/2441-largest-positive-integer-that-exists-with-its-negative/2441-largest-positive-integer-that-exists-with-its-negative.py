c66dc77c3601fd81168b0fb6f8225859b1ea8f78
nayoung <97019549+fjeos@users.noreply.github.com>
2024-05-02 22:14:47 +0900

    [Easy] Title: 2441. Largest Positive Integer That Exists With Its Negative, Time: 238 ms (12.95%), Space: 16.7 MB (77.07%) - LeetHub

--- LeetCode/Easy/2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.py
+++ LeetCode/Easy/2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.py
@@ -1,9 +1,4 @@
 class Solution:
     def findMaxK(self, nums: List[int]) -> int:
-        answer = -1
-        for num in nums:
-            if -num in nums:
-                if answer < num:
-                    answer = -num if num < 0 else num
-        return answer
-        
+        result = sorted([-num if num < 0 else num for num in nums if -num in nums], reverse = True)
+        return result[0] if result else -1
