4926c91c6c67cc5b5eb5eb1f925daec276feb589
nayoung <97019549+fjeos@users.noreply.github.com>
2024-07-13 16:50:42 +0900

    [Medium] Title: 1190. Reverse Substrings Between Each Pair of Parentheses, Time: 28 ms (93.00%), Space: 16.5 MB (52.33%) - LeetHub

--- LeetCode/Medium/1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.py
+++ LeetCode/Medium/1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.py
@@ -2,15 +2,13 @@ class Solution:
     def reverseParentheses(self, s: str) -> str:
         
         queue = deque()
-        result = []
-        
-        for char in s:
-            if char == "(":
-                queue.append(len(result))
-            elif char == ")":
+        s = list(s)
+
+        for i in range(len(s)):
+            if s[i] == "(":
+                queue.append(i+1)
+            elif s[i] == ")":
                 start = queue.pop()
-                result[start:] = result[start:][::-1]
-            else:
-                result.append(char)
-                
-        return ''.join(result)
+                s[start:i] = s[i-1:start-1:-1]
+
+        return "".join(s).replace("(","").replace(")", "")
