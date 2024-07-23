2eeed7783d425fddb2c86f55864da240ee9dffd1
nayoung <97019549+fjeos@users.noreply.github.com>
2024-07-24 01:34:58 +0900

    [Gold V] Title: 토마토, Time: 412 ms, Memory: 156756 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,3 +1,5 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
 from collections import deque
 
 M, N = map(int, input().split())
@@ -5,7 +7,7 @@ tomato_box = [list(map(int, input().split())) for _ in range(N)]
 
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
-queue = deque()
+
 matured = deque()
 
 for col in range(N):
@@ -13,24 +15,23 @@ for col in range(N):
         if tomato_box[col][row] == 1:
             matured.append((col, row))
 
-days = 0
 while matured:
-    while matured:
-        queue.append(matured.popleft())
-    while queue:
-        col, row = queue.popleft()
+    col, row = matured.popleft()
 
-        for n in range(4):
-            n_row = row + dx[n]
-            n_col = col + dy[n]
-            if 0 <= n_row < M and 0 <= n_col < N and tomato_box[n_col][n_row] == 0:
-                matured.append((n_col, n_row))
-                tomato_box[n_col][n_row] = tomato_box[col][row] + 1
-    days += 1
+    for n in range(4):
+        n_row = row + dx[n]
+        n_col = col + dy[n]
+        if 0 <= n_row < M and 0 <= n_col < N and tomato_box[n_col][n_row] == 0:
+            matured.append((n_col, n_row))
+            tomato_box[n_col][n_row] = tomato_box[col][row] + 1
 
+
+days = 0
 for tomatoes in tomato_box:
-    for tomato in tomatoes:
-        if tomato == 0:
-            print(-1)
-            exit(0)
+    if 0 in tomatoes:
+        print(-1)
+        exit(0)
+    else:
+        days = max(days, max(tomatoes))
+        
 print(days - 1)
