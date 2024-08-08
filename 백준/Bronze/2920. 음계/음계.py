ff7a08503387350f810254d62a26e5c3d213ba64
nayoung <97019549+fjeos@users.noreply.github.com>
2024-08-08 23:26:16 +0900

    [Bronze II] Title: 음계, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,10 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+numbers = list(map(int, input().split()))
+if numbers == sorted(numbers):
+    print("ascending")
+elif numbers == sorted(numbers, reverse=True):
+    print("descending")
+else:
+    print("mixed")
