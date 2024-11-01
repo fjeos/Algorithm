77106fb1a118619cb1d9e4ef3b9db876ec3ae721
nayoung <97019549+fjeos@users.noreply.github.com>
2024-11-02 00:45:46 +0900

    [Silver III] Title: 서강근육맨, Time: 44 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/20300. 서강근육맨/서강근육맨.py
+++ 백준/Silver/20300. 서강근육맨/서강근육맨.py
@@ -1,27 +1,19 @@
 import sys
-input = lambda: sys.stdin.readline()
+input = lambda: sys.stdin.readline().rstrip()
 
 N = int(input())
-loss = list(map(int, input().split()))
-loss.sort()
-result = 0
+PT = list(map(int, input().split()))
+PT.sort()
 
-
-def find_max_loss(start, end):
-    global result
-    while start < end:
-        result = max(result, loss[end] + loss[start])
-        start += 1
-        end -= 1
-
-
-# 운동 기구가 짝수개이면
 if N % 2 == 0:
-    find_max_loss(0, N - 1)
-
-# 운동 기구가 홀수개이면
+    result, end = 0, N - 1
 else:
-    find_max_loss(0, N - 2)
-    result = max(result, loss[-1])
+    result, end = PT[-1], N - 2
+
+start = 0
+while start < end:
+    result = max(result, PT[start] + PT[end])
+    start += 1
+    end -= 1
 
 print(result)
