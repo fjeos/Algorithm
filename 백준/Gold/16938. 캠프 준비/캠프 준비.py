fa9cd16d975838662059240d6cae24563d6288d4
nayoung <97019549+fjeos@users.noreply.github.com>
2024-11-03 22:46:36 +0900

    [Gold V] Title: 캠프 준비, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/16938. 캠프 준비/캠프 준비.py
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.py
@@ -3,14 +3,13 @@ input = lambda: sys.stdin.readline().rstrip()
 
 from itertools import combinations
 num_problems, least_level, max_level, diff_level = map(int, input().split())
-levels = list(map(int, input().split()))
-levels.sort()
+levels = sorted(list(map(int, input().split())))
+
 answer = 0
 
 for i in range(2, num_problems + 1):
-    result = list(combinations(levels, i))
-    for pairs in result:
-        if least_level <= sum(pairs) <= max_level and max(pairs) - min(pairs) >= diff_level:
+    for pairs in combinations(levels, i):
+        if least_level <= sum(pairs) <= max_level and pairs[-1] - pairs[0] >= diff_level:
             answer += 1
 
 print(answer)
