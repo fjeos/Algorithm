33385a859e0ec67e816afb02d667e5fb14180940
nayoung <97019549+fjeos@users.noreply.github.com>
2024-12-21 21:43:01 +0900

    [Silver I] Title: 절댓값 힙, Time: 136 ms, Memory: 42144 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,16 +1,11 @@
-import sys
+import heapq, sys
+
 input = lambda: sys.stdin.readline().rstrip()
 
-import heapq
 heap = []
 for _ in range(int(input())):
-    num = int(input())
-    if num != 0:
-        heapq.heappush(heap, (abs(num), num))
+    x = int(input())
+    if x == 0:
+        print(heapq.heappop(heap)[1] if heap else 0)
     else:
-        if len(heap) == 0:
-            print(0)
-            continue
-        min_value, value = heapq.heappop(heap)
-        print(value)
-
+        heapq.heappush(heap, (abs(x), x))
