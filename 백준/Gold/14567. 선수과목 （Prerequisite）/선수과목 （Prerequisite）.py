82991cf60a609a8423ea43aceae62f64f836d760
nayoung <97019549+fjeos@users.noreply.github.com>
2025-01-28 00:28:56 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 676 ms, Memory: 51196 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
@@ -9,11 +9,8 @@ for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
 
-visited = [False] * (n + 1)
 for i in range(1, n + 1):
-    visited[i] = True
     for el in graph[i]:
         result[el] = max(result[i] + 1, result[el])
-        visited[el] = True
 
 print(" ".join(map(str, result[1:])))
