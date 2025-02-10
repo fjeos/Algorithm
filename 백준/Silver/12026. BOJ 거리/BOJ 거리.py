8b057672ece1dd33796ad78f895b3e6b54f082f8
nayoung <97019549+fjeos@users.noreply.github.com>
2025-02-10 15:39:55 +0900

    [Silver I] Title: BOJ 거리, Time: 188 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/12026. BOJ 거리/BOJ 거리.py
+++ 백준/Silver/12026. BOJ 거리/BOJ 거리.py
@@ -3,14 +3,15 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 N = int(input())
-road = input()
+blocks = input()
 dp = [1e9] * N
 dp[0] = 0
 for i in range(1, N):
-    for j in range(i):
-        if road[i] == 'B' and road[j] == 'J' or \
-            road[i] == 'O' and road[j] == 'B' or \
-            road[i] == 'J' and road[j] == 'O':
-                dp[i] = min(dp[i], (j - i) ** 2 + dp[j])
-
-print(dp[N - 1] if dp[N-1] != 1e9 else -1)
+    for j in range(i + 1):
+        if blocks[i] == 'B' and blocks[j] == 'J':
+            dp[i] = min(dp[i], dp[j] + abs(i - j) ** 2)
+        elif blocks[i] == 'O' and blocks[j] == 'B':
+            dp[i] = min(dp[i], dp[j] + abs(i - j) ** 2)
+        elif blocks[i] == 'J' and blocks[j] == 'O':
+            dp[i] = min(dp[i], dp[j] + abs(i - j) ** 2)
+print(dp[N-1] if dp[N-1] != 1e9 else -1)
