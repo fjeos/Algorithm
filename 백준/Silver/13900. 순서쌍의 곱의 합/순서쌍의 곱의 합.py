97de0419a41b2d459fb8c2dc05e6199c38e006d2
nayoung <97019549+fjeos@users.noreply.github.com>
2025-02-11 22:19:03 +0900

    [Silver IV] Title: 순서쌍의 곱의 합, Time: 88 ms, Memory: 42660 KB -BaekjoonHub

--- 백준/Silver/13900. 순서쌍의 곱의 합/순서쌍의 곱의 합.py
+++ 백준/Silver/13900. 순서쌍의 곱의 합/순서쌍의 곱의 합.py
@@ -1,18 +1,12 @@
 import sys
+
 input = lambda: sys.stdin.readline().rstrip()
+
 N = int(input())
-nums = list(map(int, input().split()))
-
-acc = [sum(nums[1:])]
-answer = [0 for _ in range(N)]
-for i in range(1, N):
-    acc.append(acc[i - 1] - nums[i])
-
+numbers = list(map(int, input().split()))
+total = sum(numbers)
+answer = 0
 for i in range(N):
-    acc[i] = acc[i] * nums[i]
-
-answer[0] = acc[0]
-for j in range(1, N):
-    answer[j] = answer[j - 1] + acc[j]
-
-print(answer[-1])
+    total -= numbers[i]
+    answer += total * numbers[i]
+print(answer)
