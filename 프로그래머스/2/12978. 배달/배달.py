e6b0e981edf6b965f18686182375fe05424b4358
nayoung <97019549+fjeos@users.noreply.github.com>
2025-02-11 21:11:49 +0900

    [level 2] Title: 배달, Time: 4.47 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,23 +1,20 @@
-from collections import deque
+import heapq
 def solution(N, road, K):
-    maps = [{} for _ in range(N + 1)]
-    dist = [K + 1] * (N + 1)
-    dist[1] = 0
-    success = {1}
-
-    for a, b, c in road:
-        if b not in maps[a] or maps[a][b] > c:
-            maps[a][b] = c
-            maps[b][a] = c
-
-    queue = deque([1])
-
-    while queue:
-        a = queue.popleft()
-        for b, c in maps[a].items():
-            if dist[b] > c + dist[a]:
-                dist[b] = dist[a] + c
-                queue.append(b)
-                success.add(b)
-
-    return len(success)
+    graph = [[] for i in range(N + 1)]
+    for info in road:
+        graph[info[0]].append([info[1], info[2]])
+        graph[info[1]].append([info[0], info[2]])
+    dp = [1e9] * (N + 1)
+    dp[1] = 0
+    heap = []
+    heapq.heappush(heap, (1, 0))
+    while heap:
+        curr_node, curr_dist = heapq.heappop(heap)
+        if curr_dist <= dp[curr_node]:
+            for node, dist in graph[curr_node]:
+                next_dist = curr_dist + dist
+                if next_dist < dp[node]:
+                    dp[node] = next_dist
+                    heapq.heappush(heap, (node, next_dist))
+    
+    return len([i for i in dp if i <= K])
