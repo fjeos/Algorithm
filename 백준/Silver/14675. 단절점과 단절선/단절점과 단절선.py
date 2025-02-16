a13aab174b736403674b8078b17f832a0e8a6089
nayoung <97019549+fjeos@users.noreply.github.com>
2025-02-17 02:08:38 +0900

    [Silver I] Title: 단절점과 단절선, Time: 268 ms, Memory: 47528 KB -BaekjoonHub

--- 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.py
+++ 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.py
@@ -1,17 +1,16 @@
 import sys
-
 input = lambda: sys.stdin.readline().rstrip()
 
 N = int(input())
-tree = [[] for _ in range(N + 1)]
-for _ in range(N-1):
-    u, v = map(int, input().split())
-    tree[u].append(v)
-    tree[v].append(u)
+graph = [[] for _ in range(N + 1)]
+for _ in range(N - 1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
 for _ in range(int(input())):
     t, k = map(int, input().split())
     if t == 1:
-        print("yes") if len(tree[k]) > 1 else print("no")
+        print("no" if len(graph[k]) < 2 else "yes")
     else:
         print("yes")
