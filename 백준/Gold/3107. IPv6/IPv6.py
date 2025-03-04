9a6c269394e7c3139a84d80d2241efcf0113db1a
nayoung <97019549+fjeos@users.noreply.github.com>
2025-03-04 23:48:28 +0900

    [Gold V] Title: IPv6, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/3107. IPv6/IPv6.py
+++ 백준/Gold/3107. IPv6/IPv6.py
@@ -1,30 +1,24 @@
 import sys
 
+input = lambda: sys.stdin.readline().rstrip()
 
-def normalize_ipv6(v6):
-    # 특수한 경우: 전체가 0인 경우
-    if v6 == '::':
-        return '0000:0000:0000:0000:0000:0000:0000:0000'
+v6 = input()
+comp = v6.split(":")
+if '' in comp:
+    index = comp.index('')
+if len(comp) > 8:
+    comp.pop(comp.index(''))
+    
+for i in range(len(comp)):
+    length = len(comp[i])
+    if length < 4:
+        if length == 0:
+            comp[i] = '0000'
+        else:
+            temp = '0' * (4 - length)
+            comp[i] = temp + comp[i]
 
-    # :: 위치 찾기
-    if '::' in v6:
-        left, right = v6.split('::')
-        left_parts = left.split(':') if left else []
-        right_parts = right.split(':') if right else []
-        # 0으로 채워야 할 그룹 수 계산
-        zero_groups = 8 - len(left_parts) - len(right_parts)
+while len(comp) < 8:
+    comp.insert(index, '0000')
+print(":".join(comp))
 
-        # 완전한 주소 구성
-        full_parts = left_parts + ['0000'] * zero_groups + right_parts
-    else:
-        full_parts = v6.split(':')
-
-    # 각 그룹을 4자리로 패딩
-    normalized_parts = [part.zfill(4) for part in full_parts]
-
-    return ':'.join(normalized_parts)
-
-
-# 입력 받기
-v6 = sys.stdin.readline().rstrip()
-print(normalize_ipv6(v6))
