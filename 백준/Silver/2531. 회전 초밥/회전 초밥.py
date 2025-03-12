565d03c62c6f5c4640d4ba184c23022d69d71175
nayoung <97019549+fjeos@users.noreply.github.com>
2025-03-12 14:29:11 +0900

    [Silver I] Title: 회전 초밥, Time: 96 ms, Memory: 35352 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -1,35 +1,41 @@
 import sys
-from collections import deque
-
+from collections import defaultdict
 input = lambda: sys.stdin.readline().rstrip()
 
 n, d, k, c = map(int, input().split())
 belt = [int(input()) for _ in range(n)]
+belt = belt + belt[:k-1]  # 원형으로 처리하기 위해 필요한 부분만 추가
 
-# 벨트를 2배로 늘려 원형 회전을 구현
-belt += belt  
+# 슬라이딩 윈도우 구현
+answer = 0
+counter = defaultdict(int)
+
 
 # 초기 윈도우 설정
-kinds = deque(belt[:k])
-unique_sushi = set(kinds)
-answer = len(unique_sushi) + (1 if c not in unique_sushi else 0)
+for i in range(k):
+    counter[belt[i]] += 1
 
-# 슬라이딩 윈도우 시작
-for start in range(1, n):  # 1부터 시작
-    end = start + k - 1  # 마지막 초밥 위치
+# 쿠폰 고려한 초기값 계산
+if c not in counter:
+    answer = len(counter) + 1
+else:
+    answer = len(counter)
 
-    # 왼쪽 초밥 제거
-    removed_sushi = kinds.popleft()
-    if removed_sushi not in kinds:  # 더 이상 남아있지 않다면 제거
-        unique_sushi.remove(removed_sushi)
-
-    # 오른쪽 초밥 추가
-    added_sushi = belt[end]
-    kinds.append(added_sushi)
-    unique_sushi.add(added_sushi)
-
-    # 쿠폰 초밥 추가 여부 확인
-    current_count = len(unique_sushi) + (1 if c not in unique_sushi else 0)
-    answer = max(answer, current_count)
+# 슬라이딩 윈도우 이동
+for i in range(k, n+k-1):
+    # 이전 초밥 제거
+    counter[belt[i-k]] -= 1
+    if counter[belt[i-k]] == 0:
+        del counter[belt[i-k]]
+    
+    # 새 초밥 추가
+    counter[belt[i]] += 1
+    
+    # 현재 윈도우에서 가능한 최대 종류 계산
+    current = len(counter)
+    if c not in counter:
+        current += 1
+        
+    answer = max(answer, current)
 
 print(answer)
