7e3cba4965fe4288015a2be984408172330b5395
nayoung <97019549+fjeos@users.noreply.github.com>
2025-03-27 22:56:00 +0900

    [Silver III] Title: 동전 게임, Time: 176 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10837. 동전 게임/동전 게임.py
+++ 백준/Silver/10837. 동전 게임/동전 게임.py
@@ -1,20 +1,16 @@
-rounds = int(input())
+import sys
+
+input = sys.stdin.readline
+
+K = int(input())
+
 for _ in range(int(input())):
     M, N = map(int, input().split())
-    rest_rounds = rounds - max(M, N)
-    diff = abs(M - N)
-    # 영희와 동수의 점수가 같을 때
     if M == N:
-        print(1)
-        continue
-    # 영희의 점수가 동수보다 클 때
-    elif M > N:
-        if diff - rest_rounds <= 2:
-            print(1)
-            continue
-    # 동수의 점수가 영희보다 클 때
+        answer = 1
     elif M < N:
-        if diff - rest_rounds <= 1:
-            print(1)
-            continue
-    print(0)
+        answer = 1 if (N - M) - (K - N) <= 1 else 0
+    else:
+        answer = 1 if (M - N) - (K - M) <= 2 else 0
+
+    print(answer)
