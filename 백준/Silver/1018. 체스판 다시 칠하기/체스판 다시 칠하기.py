95630bb01a96c56cc2f6cf66a8da7049ba70db43
nayoung <97019549+fjeos@users.noreply.github.com>
2025-04-20 16:32:41 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 52 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,26 +1,27 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
 N, M = map(int, input().split())
-board = []
-result = []
-for _ in range(N):
-    board.append(input())
-    
-for i in range(N-7): 
-    for j in range(M-7):
-        countW = 0
-        countB = 0
-        for k in range(8):
-            for l in range(8):
-                if (k+l) % 2 == 0:
-                    if board[i+k][j+l] != 'W':
-                        countW += 1
-                    if board[i+k][j+l] != 'B':
-                        countB += 1
-                else:
-                    if board[i+k][j+l] != 'B':
-                        countW += 1
-                    if board[i+k][j+l] != 'W':
-                        countB += 1
-        result.append(countW)
-        result.append(countB)      
-        
-print(min(result))               
+boards = [list(input()) for _ in range(N)]
+W_boards = ['WBWBWBWB', 'BWBWBWBW'] * 4
+B_boards = ['BWBWBWBW', 'WBWBWBWB'] * 4
+
+
+def check_boards(arr):
+    w_count, b_count = 0, 0
+    for k in range(8):
+        for s in range(8):
+            if arr[k][s] != W_boards[k][s]:
+                w_count += 1
+            if arr[k][s] != B_boards[k][s]:
+                b_count += 1
+    return min(w_count, b_count)
+
+
+min_value = sys.maxsize
+for i in range(N - 7):
+    for j in range(M - 7):
+        min_value = min(min_value, check_boards([row[j:j + 8] for row in boards[i:i + 8]]))
+
+print(min_value)
