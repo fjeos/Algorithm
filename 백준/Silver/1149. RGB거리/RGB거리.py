eba47e1058759187fd4debc1a29a216be88eec56
nayoung <97019549+fjeos@users.noreply.github.com>
2025-07-27 22:04:06 +0900

    [Silver I] Title: RGB거리, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,12 +1,18 @@
 import sys
-input = sys.stdin.readline
+
+input = lambda: sys.stdin.readline().rstrip()
 
 N = int(input())
 RGB = [list(map(int, input().split())) for _ in range(N)]
 
+dp = [[0 for _ in range(N)] for _ in range(3)]
+dp[0][0] = RGB[0][0]
+dp[1][0] = RGB[0][1]
+dp[2][0] = RGB[0][2]
+
 for i in range(1, N):
-    RGB[i][0] = min(RGB[i-1][1], RGB[i-1][2]) + RGB[i][0]
-    RGB[i][1] = min(RGB[i-1][0], RGB[i-1][2]) + RGB[i][1]
-    RGB[i][2] = min(RGB[i-1][0], RGB[i-1][1]) + RGB[i][2]
-    
-print(min(RGB[N-1][0], RGB[N-1][1], RGB[N-1][2]))
+    dp[0][i] = min(dp[1][i-1], dp[2][i-1]) + RGB[i][0]
+    dp[1][i] = min(dp[0][i-1], dp[2][i-1]) + RGB[i][1]
+    dp[2][i] = min(dp[0][i-1], dp[1][i-1]) + RGB[i][2]
+
+print(min(dp[0][N-1], dp[1][N-1], dp[2][N-1]))
