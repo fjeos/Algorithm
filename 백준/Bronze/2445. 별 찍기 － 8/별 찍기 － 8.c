b869053a2d3a7b358d535c15bd01bb6235ee9b86
nayoung <97019549+fjeos@users.noreply.github.com>
2025-10-31 19:44:30 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(void) {
+    int i,j,x;
+    scanf("%d", &x);
+
+    for (i = 1; i <= x; i++) {
+        for (j = 0; j < i; j++) {
+            printf("*");
+        }
+        for (j = 0; j < 2 * (x - i); j++) {
+            printf(" ");
+        }
+        for (j = 0; j < i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (i = 1; i < x; i++) {
+        for (j = 0; j < x - i; j++) {
+            printf("*");
+
+        }
+        for (j = 0; j < 2 * i; j++) {
+            printf(" ");
+        }
+        for (j = 0; j < x - i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
