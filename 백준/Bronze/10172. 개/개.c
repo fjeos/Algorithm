4d181e2389252509a4ae678cbdaeb6acad92410d
nayoung <97019549+fjeos@users.noreply.github.com>
2026-01-09 22:39:17 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("|");
+    printf("\\");
+    printf("_/|");
+    printf("\n|q p|   /}");
+    printf("\n( 0 )");
+    printf("\"");
+    printf("\"");
+    printf("\"");
+    printf("\\");
+    printf("\n|");
+    printf("\"");
+    printf("^");
+    printf("\"");
+    printf("`    |");
+    printf("\n||_/=");
+    printf("\\");
+    printf("\\");
+    printf("__|");
+    return 0;
+}
