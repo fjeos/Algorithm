7e69cba165d1540811344df8b169067ff8a4cf52
nayoung <97019549+fjeos@users.noreply.github.com>
2026-02-02 14:33:43 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) {
+    int i,j,x;
+    scanf("%d", &x);
+
+    for (i = 1; i <= x; i++) {
+        for (j = 0; j < x-i; j++) {
+            printf(" ");
+        }
+        for (j = 0; j < i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
