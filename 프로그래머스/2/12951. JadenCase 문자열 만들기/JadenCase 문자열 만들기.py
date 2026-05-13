e0294966bad4256cb77753f4492b8bd7bc18b325
nayoung <97019549+fjeos@users.noreply.github.com>
2026-05-13 17:28:15 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.03 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,18 +1,10 @@
 def solution(s):
-    s = s.lower()
-    is_space = False
-    string = ""
-    for i in range(len(s)):
-        if s[i] == " ":
-            is_space = True
-            string += " "
-            continue
-        if is_space or i == 0:
-            if s[i].isalpha():
-                string += s[i].replace(s[i], s[i].upper())
-                is_space = False
-                continue
-        string += s[i]
-        is_space = False
-        
-    return string
+    answer = []
+    
+    for i, ch in enumerate(s):
+        if i == 0 or s[i - 1] == ' ':
+            answer.append(ch.upper())
+        else:
+            answer.append(ch.lower())
+            
+    return ''.join(answer)
